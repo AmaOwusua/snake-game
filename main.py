cbde1120dcbe65de69fd8cdafd532d4fbd4f407b
AmaOwusua <amaowusuaduah14@gmail.com>
2022-03-26 23:33:33 -0400

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Hello Git')
+import curses
+
+#setup window
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)  #column, row (y,x, startpoint, startpoint)
+win.keypad(1)                      #arrow keys to ctrl snake
+curses.noecho()                    #no listening to other inputs
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1)
+
+
+#game logic
+score = 0
+
+while True:
+    event = win.getch()
+print(f"Final score ={score}")
+
+
+
+
+
+
+
+
+
