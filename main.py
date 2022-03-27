ccffcb8c3ea5d20675a36f0585464c1e467dc343
AmaOwusua <amaowusuaduah14@gmail.com>
2022-03-27 01:33:40 -0400

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import curses
+import _curses
+
 
 #setup window
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)  #column, row (y,x, startpoint, startpoint)
+_curses.initscr()
+win = _curses.newwin(20, 60, 0, 0)  #column, row (y,x, startpoint, startpoint)
 win.keypad(1)                      #arrow keys to ctrl snake
-curses.noecho()                    #no listening to other inputs
-curses.curs_set(0)
+_curses.noecho()                    #no listening to other inputs
+_curses.curs_set(0)
 win.border(0)
 win.nodelay(1)
 
@@ -15,6 +16,7 @@ score = 0
 
 while True:
     event = win.getch()
+    # ...
 print(f"Final score ={score}")
 
 
