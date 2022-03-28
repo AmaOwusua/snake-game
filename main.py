21003b28eedd5a0cbcbb174b148c64623a143857
AmaOwusua <amaowusuaduah14@gmail.com>
2022-03-28 09:21:21 -0400

    library update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import _curses
+import curses
 
 
 #setup window
-_curses.initscr()
-win = _curses.newwin(20, 60, 0, 0)  #column, row (y,x, startpoint, startpoint)
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)  #column, row (y,x, startpoint, startpoint)
 win.keypad(1)                      #arrow keys to ctrl snake
-_curses.noecho()                    #no listening to other inputs
-_curses.curs_set(0)
+curses.noecho()                    #no listening to other inputs
+curses.curs_set(0)
 win.border(0)
 win.nodelay(1)
 
